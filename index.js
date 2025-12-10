353d8aa5041a989dc2ccc5b3b68a42d94d668e5c
scain-ui <scain@midland.k12.ia.us>
2025-12-10 16:34:54 +0000

    js and css done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Put your code here
+let total = 0;
+
+function addToCart(name, price) {
+    const list = document.getElementById("cart-list");
+
+    const li = document.createElement("li");
+    li.textContent = `${name} - $${price}`;
+    list.appendChild(li);
+   total += price;
+    document.getElementById("total").textContent = total;
+}
